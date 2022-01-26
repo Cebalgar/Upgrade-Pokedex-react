326ce3bc8e9cd3a95276700f911e428b5fea24f3
Celia Ballesteros <cebalgar@gmail.com>
2022-01-26 17:12:00 +0100

    Upgrade-Pokedex-react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import AppPokedex from './components/AppPokedex/AppPokedex';
+
+const App= () => {
+  return (
+    
+    <div className="App">
+    <AppPokedex/>
+   
+
+   
+
+      
+    </div>
+  );
+}
+
+export default App;
